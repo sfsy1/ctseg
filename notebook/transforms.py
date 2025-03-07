7cb5723d7538c420f0dea86c69484ad4a00fa9bf
gnefihs <sfsy1@yahoo.com>
2025-03-07 11:31:07 +0100

    Create transforms.py

---
+++ notebook/transforms.py
@@ -0,0 +1,40 @@
+from functools import partial
+
+import albumentations as A
+import numpy as np
+from albumentations.pytorch import ToTensorV2
+
+VALUE_MIN = -500
+VALUE_MAX = 1000
+
+
+def clip_image(image, min_val, max_val, **kwargs):
+    return np.clip(image, min_val, max_val)
+
+
+def scale_image(image, min_val, max_val, **kwargs):
+    return (image - min_val) / (max_val - min_val)
+
+
+def pass_through(mask, **kwargs):
+    return mask
+
+
+clip_partial = partial(clip_image, min_val=VALUE_MIN, max_val=VALUE_MAX)
+scale_partial = partial(scale_image, min_val=VALUE_MIN, max_val=VALUE_MAX)
+
+val_transform = A.Compose([A.Lambda(image=clip_partial, mask=pass_through),
+                           A.Lambda(image=scale_partial, mask=pass_through), ToTensorV2(), ])
+
+train_transform = A.Compose([A.Lambda(image=clip_partial, mask=pass_through),
+                             A.Lambda(image=scale_partial, mask=pass_through),
+
+                             # pixel
+                             A.GaussianBlur(blur_limit=5, p=0.5),
+                             A.GaussNoise(std_range=(0.01, 0.05), per_channel=False, p=0.5),
+
+                             # # spatial
+                             A.HorizontalFlip(p=0.5), A.VerticalFlip(p=0.5), A.Transpose(p=0.5),
+                             A.OpticalDistortion(distort_limit=(-0.5, -0.1), p=0.5),
+                             A.GridDistortion(p=0.5),  # produces black border at the btm/right
+                             ToTensorV2(), ])
