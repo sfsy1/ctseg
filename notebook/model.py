7b6a3050bd736411b1d1e6f70c1e9c24b03f05f2
gnefihs <sfsy1@yahoo.com>
2025-03-18 12:41:55 +0100

    lint

--- notebook/model.py
+++ notebook/model.py
@@ -1,23 +1,15 @@
-import os
-import torch
-from torch import nn
-import torch.nn.functional as F
-from torchvision import transforms
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader
 import lightning as L
-
 import segmentation_models_pytorch as smp
-from torchmetrics.segmentation import DiceScore, MeanIoU
+import torch
 from torchmetrics.regression import MeanSquaredError as MSE
-from evaluation.eval_utils import volume_similarity
+from torchmetrics.segmentation import DiceScore, MeanIoU
 
 
 class UNet(L.LightningModule):
     def __init__(self):
         super().__init__()
         self.model = smp.Unet(
-            encoder_name="resnet18", # resnext50_32x4d",
+            encoder_name="resnet18",  # resnext50_32x4d",
             encoder_weights="imagenet",
             in_channels=2,
             classes=1,
@@ -30,10 +22,10 @@ class UNet(L.LightningModule):
     def forward(self, x):
         return self.modal(x)
 
-
     def training_step(self, batch, batch_idx):
         x = batch["input"]
         y = batch["mask"]
+
         y_pred = self.model(x)
 
         # loss
@@ -50,6 +42,7 @@ class UNet(L.LightningModule):
     def validation_step(self, batch, batch_idx):
         x = batch["input"]
         y = batch["mask"]
+
         y_pred = self.model(x)
 
         # loss
@@ -58,12 +51,12 @@ class UNet(L.LightningModule):
 
         # metrics
         # use input box channel to mask pred
-        y_pred_mask = (y_pred * x[:,1:2,:,:]) > 0.5
+        y_pred_mask = (y_pred * x[:, 1:2, :, :]) > 0.5
         dice_score = self.dice(y_pred_mask, y)
 
         # calculate vol similarity at the sample level, then mean
-        pred_vols = y_pred_mask.sum(axis=(2,3))
-        y_vols = y.sum(axis=(2,3))
+        pred_vols = y_pred_mask.sum(axis=(2, 3))
+        y_vols = y.sum(axis=(2, 3))
 
         mean_vol_sim = 1 - ((pred_vols - y_vols).abs() / y_vols).mean()
         mean_vol_sim = mean_vol_sim.clip(0, 1)
