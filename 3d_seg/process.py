61c4db068f0afd6420a3ebbb86db7a8fcf6b2b4a
gnefihs <sfsy1@yahoo.com>
2025-04-11 10:13:37 +0200

    Create process.py

---
+++ 3d_seg/process.py
@@ -0,0 +1,28 @@
+from monai.transforms import (
+    Compose, LoadImage, Orientation,
+    ScaleIntensityRange, Activations, AsDiscrete,
+    Invert, Spacing
+)
+
+
+preprocess_seg = Compose([
+    LoadImage(ensure_channel_first=True),
+    Orientation(axcodes="SPL"),
+    Spacing((2.667, 1, 1), mode="bilinear"),
+])
+
+postprocess_seg = Invert(preprocess_seg)
+
+preprocess = Compose([
+    LoadImage(ensure_channel_first=True),
+    Orientation(axcodes="SPL"),
+    Spacing((2.667, 1, 1), mode="bilinear"),
+    ScaleIntensityRange(-1024, 2048, 0, 1, clip=True),
+])
+
+postprocess = Compose([
+    Activations(softmax=True),
+    AsDiscrete(argmax=True),
+    Invert(preprocess)
+    # KeepLargestConnectedComponent(),
+])
